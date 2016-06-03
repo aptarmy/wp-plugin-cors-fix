ab3f819c6bc4f6b7c3b361d179b78867dd53bb4f
aptarmy <apt.enjoy@gmail.com>
2016-06-03 18:03:24 +0700

    fix some bugs

--- corsfix.php
+++ corsfix.php
@@ -2,29 +2,18 @@
 /*
 Plugin Name: CORS Fix
 Description: Fix CORS error to allow all origin
-Author: Mr.Pakpoom Tiwakornkit
+Author: Arm
 Version: 0.0.1
 */
 
-/*add_filter( 'wp_headers', array( 'apt_cors_fix' ), 11, 1 );
-function apt_cors_fix( $headers ) {
-    header("Access-Control-Allow-Origin: " . get_http_origin());
-    header("Access-Control-Allow-Credentials: true");
-
-    // Access-Control headers are received during OPTIONS requests
-    if ( 'OPTIONS' == $_SERVER['REQUEST_METHOD'] ) {
-		header("Access-Control-Allow-Headers: DNT,Authorization,X-CSRFToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type");
-		header("Access-Control-Allow-Methods: HEAD,GET,POST,PATCH,PUT,DELETE,OPTIONS");
-    }
-    return $headers;
-}*/
-
 function cors_fix(){
-	header("Access-Control-Allow-Origin: *");
+	header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
 	header("Access-Control-Allow-Credentials: true");
 	if ( 'OPTIONS' == $_SERVER['REQUEST_METHOD'] ) {
+		header("HTTP/1.1 200 OK");
 		header("Access-Control-Allow-Headers: DNT,Authorization,X-CSRFToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type");
 		header("Access-Control-Allow-Methods: HEAD,GET,POST,PATCH,PUT,DELETE,OPTIONS");
+		exit();
 	}
 }
 add_action('init', 'cors_fix');
